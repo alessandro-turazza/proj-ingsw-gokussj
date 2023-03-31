52c1901417337e4771470b89119df6a792a37295
alessandro-turazza <alessandro.turazza@mail.polimi.it>
2023-03-31 16:15:05 +0200

    Implemented and Tested Game Data

--- src/main/java/it/polimi/ingsw/game_data/GameData.java
+++ src/main/java/it/polimi/ingsw/game_data/GameData.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.game_data;
 
 import it.polimi.ingsw.object_card.Color;
+import it.polimi.ingsw.user.personal_goal.Costraints;
 import it.polimi.ingsw.user.personal_goal.PersonalGoalCard;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -15,25 +16,91 @@ import static it.polimi.ingsw.object_card.ObjectCard.convertToColor;
 
 public class GameData {
     private ArrayList<DataObjectCard> dataObjectCards;
-    //private ArrayList<ObjectCard> objectCards;
     private ArrayList<PersonalGoalCard> personalGoalCards;
     private int[][] plank_config;
 
-    public void loadObjectCards(String path) throws IOException, ParseException {
+    private final int DIM = 9;
+
+
+    public ArrayList<DataObjectCard> getDataObjectCards() {
+        return dataObjectCards;
+    }
+
+    public ArrayList<PersonalGoalCard> getPersonalGoalCards() {
+        return personalGoalCards;
+    }
+
+    public int[][] getPlank_config() {
+        return plank_config;
+    }
+
+    private JSONObject readJson(String path) throws IOException, ParseException {
         FileReader fr = new FileReader(path);
         JSONObject obj = (JSONObject) new JSONParser().parse(fr);
+        return obj;
+    }
+
+    public void loadObjectCards(String path) throws IOException, ParseException {
+        this.dataObjectCards = new ArrayList<>();
+        JSONObject obj = readJson(path);
         JSONArray list = (JSONArray) obj.get("cardList");
 
         for (Object o : list) {
             JSONObject cardObj = (JSONObject) o;
             int id = Integer.parseInt(cardObj.get("id").toString());
             Color color = convertToColor(cardObj.get("color").toString());
-            int numOccorrenze = Integer.parseInt(cardObj.get("numobjectCard").toString());
+            int numOccorrenze = Integer.parseInt(cardObj.get("numObjectCard").toString());
 
             DataObjectCard card = new DataObjectCard(id,color, numOccorrenze);
             dataObjectCards.add(card);
 
         }
+    }
+
+    public void loadPersonalGoals(String path) throws IOException, ParseException {
+        this.personalGoalCards = new ArrayList<>();
+        JSONObject obj = readJson(path);
+        JSONArray list = (JSONArray) obj.get("listPersonalGoals");
+        int i = 1;
+
+        for (Object o : list) {
+            JSONObject oggList = (JSONObject) o;
+            JSONArray persGoalList = (JSONArray) oggList.get("listGoal");
+            PersonalGoalCard pg = new PersonalGoalCard(i++);
+
+            for(Object ob: persGoalList){
+                JSONObject singleGoal = (JSONObject) ob;
+                Color color = convertToColor(singleGoal.get("color").toString());
+                int row = Integer.parseInt(singleGoal.get("row").toString());
+                int column = Integer.parseInt(singleGoal.get("column").toString());
+                Costraints costraint = new Costraints(row, column, color);
+                pg.getCostraints().add(costraint);
+            }
+
+            this.personalGoalCards.add(pg);
+        }
+    }
+
+    public void loadPlankConfig(String path) throws IOException, ParseException {
+        this.plank_config = new int[DIM][DIM];
+        JSONObject obj = readJson(path);
+        JSONArray list = (JSONArray) obj.get("plank");
+        int i = 0, j = 0;
+
+        for(Object ob1: list){
+            JSONObject obJs1 = (JSONObject) ob1;
+            JSONArray row = (JSONArray) obJs1.get("row");
+
+            for(Object ob2: row){
+                int cell = Integer.parseInt(ob2.toString());
+                plank_config[i][j] = cell;
+                j++;
+            }
+
+            j=0;
+            i++;
+        }
+
     }
 
 
--- src/test/java/it/polimi/ingsw/game_data_test/GameDataTest.java
+++ src/test/java/it/polimi/ingsw/game_data_test/GameDataTest.java
@@ -1,7 +1,14 @@
 package it.polimi.ingsw.game_data_test;
 
 import it.polimi.ingsw.game_data.GameData;
+import it.polimi.ingsw.object_card.Color;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameDataTest {
     private static GameData gameData;
@@ -10,5 +17,40 @@ public class GameDataTest {
     public static void initializeGameData(){
         gameData = new GameData();
     }
+
+    @Test
+    public void testLoadObjectCards() throws IOException, ParseException {
+        String path = "src/data/Object_Cards_Data.json";
+        gameData.loadObjectCards(path);
+
+        assertEquals(gameData.getDataObjectCards().get(0).getId(), 1);
+        assertEquals(gameData.getDataObjectCards().get(0).getColor(), Color.YELLOW);
+        assertEquals(gameData.getDataObjectCards().get(0).getNumObjectCard(), 7);
+
+
+    }
+    @Test
+    public void testLoadPersonalGoals() throws IOException, ParseException {
+        String path = "src/data/PersonalGoals_Data.json";
+        gameData.loadPersonalGoals(path);
+
+        assertEquals(gameData.getPersonalGoalCards().get(0).getId(), 1);
+        assertEquals(gameData.getPersonalGoalCards().get(0).getCostraints().get(0).getRow(), 0);
+        assertEquals(gameData.getPersonalGoalCards().get(0).getCostraints().get(0).getColumn(), 0);
+        assertEquals(gameData.getPersonalGoalCards().get(0).getCostraints().get(0).getColor(), Color.PINK);
+
+    }
+
+    @Test
+    public void testLoadPlankConfig() throws IOException, ParseException {
+        String path = "src/data/Plank_Config_1.json";
+        gameData.loadPlankConfig(path);
+
+        assertEquals(gameData.getPlank_config()[0][0], 0);
+        assertEquals(gameData.getPlank_config()[0][3], 3);
+        assertEquals(gameData.getPlank_config()[1][3], 2);
+
+    }
+
 
 }
