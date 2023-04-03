c165764ed7c12d49b71184abd747dd630bd22465
Nick <nicolo.giallongo@mail.polimi.it>
2023-04-03 12:37:43 +0200

    Correzione RuleCommonV

--- src/main/java/it/polimi/ingsw/common_goal/rule_common/RuleCommonV.java
+++ src/main/java/it/polimi/ingsw/common_goal/rule_common/RuleCommonV.java
@@ -9,7 +9,9 @@ public class RuleCommonV implements RuleCommon {
     public boolean newRule(User user) {
         CellShelf[][] bookshelf=user.getBookshelf().getBookshelf();
         int maxrow=user.getBookshelf().getNumRow()-1,maxcolumn=user.getBookshelf().getNumColumn()-1;
-        if(bookshelf[0][0]==null || bookshelf[0][maxcolumn]==null ||bookshelf[maxrow][0]==null ||bookshelf[maxrow][maxcolumn]==null )
+        if(bookshelf[0][0]==null || bookshelf[0][maxcolumn]==null ||bookshelf[maxrow][0]==null ||bookshelf[maxrow][maxcolumn]==null)
+            return false;
+        if(bookshelf[0][0].getObjectCard()==null || bookshelf[0][maxcolumn].getObjectCard()==null ||bookshelf[maxrow][0].getObjectCard()==null ||bookshelf[maxrow][maxcolumn].getObjectCard()==null )
             return false;
         return (bookshelf[0][0].getObjectCard().getColor()==bookshelf[0][maxcolumn].getObjectCard().getColor() && bookshelf[0][maxcolumn].getObjectCard().getColor()==bookshelf[maxrow][maxcolumn].getObjectCard().getColor() && bookshelf[maxrow][maxcolumn].getObjectCard().getColor()==bookshelf[maxrow][0].getObjectCard().getColor());
 
