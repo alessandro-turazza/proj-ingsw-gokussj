349b93659ae545f1d360af6b1f0204888910fcbb
alessandro-turazza <alessandro.turazza@mail.polimi.it>
2023-05-10 11:46:16 +0200

    Background

--- src/main/java/it/polimi/ingsw/client/view/GUI.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI.java
@@ -5,10 +5,15 @@ import it.polimi.ingsw.server.model.plank.CellPlank;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.json.simple.JSONObject;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
+
+import static javafx.scene.layout.BackgroundRepeat.NO_REPEAT;
 
 public class GUI extends Application implements View{
 
@@ -110,8 +115,15 @@ public class GUI extends Application implements View{
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("hello-view.fxml"));
+        Pane root = fxmlLoader.load();
+        Scene scene = new Scene(root, stage.getMaxHeight(), stage.getMaxWidth());
+
+        FileInputStream reader = new FileInputStream("src/data/17_MyShelfie_BGA/Publisher material/Display_1.jpg");
+        Image imgBackground = new Image(reader);
+        BackgroundImage backgroundImage = new BackgroundImage(imgBackground, BackgroundRepeat.NO_REPEAT, NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        root.setBackground(new Background(backgroundImage));
+
         stage.setMaximized(true);
-        Scene scene = new Scene(fxmlLoader.load(), stage.getMaxHeight(), stage.getMaxWidth());
         stage.setTitle("MyShelfie");
         stage.setScene(scene);
         stage.show();
