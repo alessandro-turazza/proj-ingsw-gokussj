e45d30467c324a16507058813e4e537cc0e436dd
Nick <nicolo.giallongo@mail.polimi.it>
2023-05-17 14:18:21 +0200

    GUI button update 2

--- src/main/java/it/polimi/ingsw/client/PersonalButton.java
+++ src/main/java/it/polimi/ingsw/client/PersonalButton.java
@@ -2,10 +2,7 @@ package it.polimi.ingsw.client;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
@@ -16,5 +13,6 @@ public class PersonalButton extends Button {
         setBackground(new Background(new BackgroundFill(Color.rgb(96, 64, 32), CornerRadii.EMPTY, Insets.EMPTY)));
         setTextFill(Color.rgb(204, 153, 102));
         setFont(new Font("Times New Roman", 20));
+        setBorder(new Border(new BorderStroke(Color.rgb(179, 119, 0),  BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
     }
 }
